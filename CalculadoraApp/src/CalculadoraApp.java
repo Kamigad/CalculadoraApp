90bf94bbaed89efb85417355cfe143f799ca34da
Kamigad <camilo971939@gmail.com>
2026-02-14 18:35:14 -0500

    agregamos las funciones y el soporte de double para las divisiones y valores decimales

--- CalculadoraApp/src/CalculadoraApp.java
+++ CalculadoraApp/src/CalculadoraApp.java
@@ -6,42 +6,13 @@ public class CalculadoraApp {
         while (true){
             System.out.println("*** Aplicacion Calculadora ***");
             // Mostramos el menu
-            System.out.println("""
-                1. Suma
-                2. Resta
-                3. Multiplicacion
-                4. Division
-                5. Salir
-                """);
-            System.out.print("Operacion a Realizar? ");
+            mostrarMenu();
             try {
                 var operacion = Integer.parseInt(consola.nextLine());
                 // Revisar que este dentro de las opciones validadas
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.print("Proporciona el primer valor: ");
-                    var operando1 = Integer.parseInt(consola.nextLine());
-                    System.out.print("Proporciona el segundo valor: ");
-                    var operando2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    switch (operacion) {
-                        case 1 -> {//Suma
-                            resultado = operando1 + operando2;
-                            System.out.println("Resultado Suma: " + resultado);
-                        }
-                        case 2 -> {//Resta
-                            resultado = operando1 - operando2;
-                            System.out.println("Resultado Resta: " + resultado);
-                        }
-                        case 3 -> {//Multiplicacion
-                            resultado = operando1 * operando2;
-                            System.out.println("Resultado Multiplicacion: " + resultado);
-                        }
-                        case 4 -> {//Division
-                            resultado = operando1 / operando2;
-                            System.out.println("Resultado Division: " + resultado);
-                        }
-                        default -> System.out.println("Opcion erronea: " + operacion);
-                    }
+                    //Ejecutamos la operacion deseada
+                    ejecutarOperacion(operacion,consola);
                 } else if (operacion == 5) {//Salir
                     System.out.println("Hasta pronto...");
                     break;
@@ -57,5 +28,43 @@ public class CalculadoraApp {
         }//fin while
 
     }//fin de main
+
+    private static void mostrarMenu(){
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operacion a Realizar? ");
+    }
+
+    private static void ejecutarOperacion(int operacion, Scanner consola){
+        System.out.print("Proporciona el primer valor: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.print("Proporciona el segundo valor: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        double resultado;
+        switch (operacion) {
+            case 1 -> {//Suma
+                resultado = operando1 + operando2;
+                System.out.println("Resultado Suma: " + resultado);
+            }
+            case 2 -> {//Resta
+                resultado = operando1 - operando2;
+                System.out.println("Resultado Resta: " + resultado);
+            }
+            case 3 -> {//Multiplicacion
+                resultado = operando1 * operando2;
+                System.out.println("Resultado Multiplicacion: " + resultado);
+            }
+            case 4 -> {//Division
+                resultado = operando1 / operando2;
+                System.out.println("Resultado Division: " + resultado);
+            }
+            default -> System.out.println("Opcion erronea: " + operacion);
+        }
+    }
 
 }//fin de class
