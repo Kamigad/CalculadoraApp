d517c966e9e56378cb5d3e529cafd6a965d727a0
Kamigad <camilo971939@gmail.com>
2026-02-14 18:20:35 -0500

    Agregamos los ciclos y excepciones a la aplicacion de Calculadora

--- CalculadoraApp/src/CalculadoraApp.java
+++ CalculadoraApp/src/CalculadoraApp.java
@@ -3,50 +3,59 @@ import java.util.Scanner;
 public class CalculadoraApp {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        System.out.println("*** Aplicacion Calculadora ***");
-        // Mostramos el menu
-        System.out.println("""
+        while (true){
+            System.out.println("*** Aplicacion Calculadora ***");
+            // Mostramos el menu
+            System.out.println("""
                 1. Suma
                 2. Resta
                 3. Multiplicacion
                 4. Division
                 5. Salir
                 """);
-        System.out.print("Operacion a Realizar? ");
-        var operacion = Integer.parseInt(consola.nextLine());
-        // Revisar que este dentro de las opciones validadas
-        if(operacion >= 1 && operacion <= 4){
-            System.out.print("Proporciona el primer valor: ");
-            var operando1 = Integer.parseInt(consola.nextLine());
-            System.out.print("Proporciona el segundo valor: ");
-            var operando2 = Integer.parseInt(consola.nextLine());
-            int resultado;
-            switch (operacion){
-                case 1 -> {//Suma
-                    resultado = operando1 + operando2;
-                    System.out.println("Resultado Suma: " + resultado);
+            System.out.print("Operacion a Realizar? ");
+            try {
+                var operacion = Integer.parseInt(consola.nextLine());
+                // Revisar que este dentro de las opciones validadas
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.print("Proporciona el primer valor: ");
+                    var operando1 = Integer.parseInt(consola.nextLine());
+                    System.out.print("Proporciona el segundo valor: ");
+                    var operando2 = Integer.parseInt(consola.nextLine());
+                    int resultado;
+                    switch (operacion) {
+                        case 1 -> {//Suma
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado Suma: " + resultado);
+                        }
+                        case 2 -> {//Resta
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado Resta: " + resultado);
+                        }
+                        case 3 -> {//Multiplicacion
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado Multiplicacion: " + resultado);
+                        }
+                        case 4 -> {//Division
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado Division: " + resultado);
+                        }
+                        default -> System.out.println("Opcion erronea: " + operacion);
+                    }
+                } else if (operacion == 5) {//Salir
+                    System.out.println("Hasta pronto...");
+                    break;
+                } else {
+                    System.out.println("Opcion erronea: " + operacion);
                 }
-                case 2 -> {//Resta
-                    resultado = operando1 - operando2;
-                    System.out.println("Resultado Resta: " + resultado);
-                }
-                case 3 ->{//Multiplicacion
-                    resultado = operando1*operando2;
-                    System.out.println("Resultado Multiplicacion: " + resultado);
-                }
-                case 4 ->{//Division
-                    resultado = operando1/operando2;
-                    System.out.println("Resultado Division: " + resultado);
-                }
-                default -> System.out.println("Opcion erronea: " + operacion);
+                //Imprimimos un salto antes de repetir el ciclo
+                System.out.println();
+            } //fin try
+            catch (Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage());
             }
-        }
-        else if (operacion == 5) {//Salir
-            System.out.println("Hasta pronto...");
-        }
-        else {
-            System.out.println("Opcion erronea: " + operacion);
-        }
+        }//fin while
 
-    }
-}
+    }//fin de main
+
+}//fin de class
